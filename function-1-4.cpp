6a96338136a471283f28aac8224d66e803ffe590
System Administrator <root@student-10-201-32-025.wireless.adelaide.edu.au>
2022-03-23 10:04:36 +1030

    adding 1-4 files

---
+++ function-1-4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int size_of_variable_star_arr(){
+    int *arr = new int[3];
+
+    int size = 0;
+
+    for (int i = 0; i < 3; i++){
+        size = size + sizeof(*(arr + i));
+    }
+
+    return size;
+
+    
+}
+
+// using namespace std;
+// #include <iostream>
+// #include <string>
+
+// int size_of_variable_star_t(){
+//     int a, *p;
+//     p=&a;
+
+//     return sizeof(p);
+// }
---
+++ main-1-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int size_of_variable_star_arr();
+
+int main(){
+
+    cout << size_of_variable_star_arr() << endl;
+}
+
+// using namespace std;
+// #include <string>
+// #include <iostream>
+
+// int size_of_variable_star_t();
+// int main() {
+     
+    
+
+//     cout << size_of_variable_star_t() << endl;
+
+// }
